f278797ba569b8205296049974f64efaa1fefe53
Luzhania Cespedes Arancibia <luzhaniacesar@gmail.com>
2025-09-30 16:13:20 -0400

    test: generate the same number for another number that doesn't have rules, like the 2

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -4,5 +4,7 @@ describe("Fizzbuzz", () => {
   it("should generate the same number for one that doesn't have rules, like the 1", () => {
     expect(generateFizzBuzz(1)).toEqual("1");
   });
-  
+  it("should generate the same number for another number that doesn't have rules, like the 2", () => {
+    expect(generateFizzBuzz(2)).toEqual("2");
+  });
 });
